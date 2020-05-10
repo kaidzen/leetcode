a476cc4e16518af0413e491dbbc90585143eb290
kaidzen <karpov.srg#gmail.com>
2020-05-10 14:28:25 +0200

    Is number perfect square Points  on strait line

---
+++ src/main/java/org/kaidzen/study/leetcode/PerfectSquare.java
@@ -0,0 +1,50 @@
+package org.kaidzen.study.leetcode;
+
+public class PerfectSquare {
+
+    public boolean isPerfectSquare2(int num) {
+        if (num < 2){
+            return true;
+        }
+
+        long start = 2;
+        long end = num/2;
+        while (start <= end){
+            long mid = start + (end - start)/2;
+            long checked = mid * mid;
+            if (checked == num){
+                return true;
+            } else if (checked > num){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPerfectSquare(int num) {
+        if (num < 2){
+            return true;
+        }
+        for (int i=2; i<=num/2; i++){
+            if (i*i == num){
+                return true;
+            } else if (i*i > num){
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        Input: 16
+//        Output: true
+        PerfectSquare prefect = new PerfectSquare();
+        System.out.println(prefect.isPerfectSquare(16));
+
+//        Input: 14
+//        Output: false
+        System.out.println(prefect.isPerfectSquare(14));
+    }
+}
---
+++ src/main/java/org/kaidzen/study/leetcode/PointsOnStraitLine.java
@@ -0,0 +1,42 @@
+package org.kaidzen.study.leetcode;
+
+public class PointsOnStraitLine {
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        int length = coordinates.length;
+        if (length == 0){
+            return false;
+        }else if (length == 2){
+            return true;
+        }
+        double x1 = coordinates[0][0];
+        double y1 = coordinates[0][1];
+
+        Double slope = null;
+        for (int i = 1; i< length; i++){
+            double x2 = coordinates[i][0];
+            double y2 = coordinates[i][1];
+
+            if (x2-x1 == 0){ return false;}
+            if (slope == null){
+                slope = (y2-y1)/(x2-x1);
+                continue;
+            }
+            double currentSlope = (y2 - y1) / (x2 - x1);
+            if (slope != currentSlope){return false;}
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+//        Output: true
+        PointsOnStraitLine points = new PointsOnStraitLine();
+        int[][] array1 = new int[][]{{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
+        System.out.println(points.checkStraightLine(array1));
+//        Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+//        Output: false
+        int[][] array2 = new int[][]{{1,1},{2,2},{3,4},{4,5},{5,6},{7,7}};
+        System.out.println(points.checkStraightLine(array2));
+    }
+}
