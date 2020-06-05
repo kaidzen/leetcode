083808ab774256cbe89bfb8336fd1e47e8a682bc
kaidzen <karpov.srg#gmail.com>
2020-06-05 20:07:43 +0200

    Revers string. Used two pointers approach.

---
+++ src/main/java/org/kaidzen/study/leetcode/june2020/ReverseString.java
@@ -0,0 +1,31 @@
+package org.kaidzen.study.leetcode.june2020;
+
+public class ReverseString {
+
+    public char[] reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while(start < end){
+            swap(s, start, end);
+            start++;
+            end--;
+        }
+        return s;
+    }
+
+    private void swap(char[] str, int one, int two){
+        char temp = str[one];
+        str[one] = str[two];
+        str[two] = temp;
+    }
+
+    public static void main(String[] args) {
+        ReverseString revers = new ReverseString();
+//        Input: ["h","e","l","l","o"]
+//        Output: ["o","l","l","e","h"]
+        System.out.println(revers.reverseString("hello".toCharArray()));
+//        Input: ["H","a","n","n","a","h"]
+//        Output: ["h","a","n","n","a","H"]
+        System.out.println(revers.reverseString("Hannah".toCharArray()));
+    }
+}
