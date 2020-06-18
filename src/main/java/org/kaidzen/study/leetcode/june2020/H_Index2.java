b4c3a18009cdcecd0d64103959a11e3d9829c562
kaidzen <karpov.srg#gmail.com>
2020-06-18 21:52:13 +0200

    Calculate H-index. Two variants, with O(n) and O(logn) complexity.

---
+++ src/main/java/org/kaidzen/study/leetcode/june2020/H_Index2.java
@@ -0,0 +1,38 @@
+package org.kaidzen.study.leetcode.june2020;
+
+public class H_Index2 {
+
+    public int hIndex2(int[] citations) {
+        int n = citations.length;
+        int left = 0;
+        int right = n-1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            if (citations[mid] < n-mid){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return n - left;
+    }
+
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int idx = n-1;
+        while (idx >= 0){
+            if (citations[idx] < n-idx){ break;}
+            idx--;
+        }
+        return n - idx - 1;
+    }
+
+    public static void main(String[] args) {
+//        Input: citations = [0,1,3,5,6]
+//        Output: 3
+
+        H_Index2 hIndexCounter = new H_Index2();
+        System.out.println(hIndexCounter.hIndex(new int[]{0,1,3,5,6}));
+        System.out.println(hIndexCounter.hIndex2(new int[]{0,1,3,5,6}));
+    }
+}
