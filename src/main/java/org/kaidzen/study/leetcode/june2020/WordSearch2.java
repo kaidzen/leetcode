075393547b904d3cff68b0493da42934d27e0939
kaidzen <karpov.srg#gmail.com>
2020-09-07 23:02:10 +0200

    Find words on board. Used Trie to store words, that DFS for search

---
+++ src/main/java/org/kaidzen/study/leetcode/june2020/WordSearch2.java
@@ -0,0 +1,66 @@
+package org.kaidzen.study.leetcode.june2020;
+
+import org.kaidzen.study.leetcode.util.Trie;
+
+import java.util.*;
+
+public class WordSearch2 {
+
+    public List<String> findWords(char[][] board, String[] words) {
+        if (words.length == 0){
+            return Collections.emptyList();
+        }
+        Trie trie = new Trie();
+        for (String word: words){
+            trie.insert(word);
+        }
+        Set<String> res = new HashSet<>();
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board[0].length; j++){
+                dfs(board, i, j, res, trie, "");
+            }
+        }
+        return new ArrayList<>(res);
+    }
+
+    private void dfs(char[][] board, int i, int j, Set<String> res, Trie trie, String currentStr) {
+        char ch = board[i][j];
+        if (ch == '$'){
+            return;
+        }
+        board[i][j] = '$';
+        Trie current = trie.getChildren()[ch-'a'];
+        if (current != null){
+            String str = currentStr + ch;
+            if (current.isEndOfWord()) {
+                res.add(str);
+            }
+            if (i < board.length-1) {dfs(board, i+1, j, res, current, currentStr);}
+            if (j < board[0].length-1) {dfs(board, i, j+1, res, current, currentStr);}
+            if (i > 0) {dfs(board, i-1, j, res, current, currentStr);}
+            if (j > 0) {dfs(board, i, j-1, res, current, currentStr);}
+        }
+        board[i][j] = ch;
+    }
+
+    public static void main(String[] args) {
+        WordSearch2 search2 = new WordSearch2();
+//        Input:
+//        board = [
+//                    ['o','a','a','n'],
+//                    ['e','t','a','e'],
+//                    ['i','h','k','r'],
+//                    ['i','f','l','v']
+//                ]
+//        words = ["oath","pea","eat","rain"]
+//
+//        Output: ["eat","oath"]
+        List<String> words = search2.findWords(new char[][]{
+                new char[]{'o', 'a', 'a', 'n'},
+                new char[]{'e', 't', 'a', 'e'},
+                new char[]{'i', 'h', 'k', 'r'},
+                new char[]{'i', 'f', 'l', 'v'}
+        }, new String[]{"oath", "pea", "eat", "rain"});
+        System.out.println(words);
+    }
+}
---
+++ src/main/java/org/kaidzen/study/leetcode/util/Trie.java
@@ -0,0 +1,60 @@
+package org.kaidzen.study.leetcode.util;
+
+import java.util.Arrays;
+
+public class Trie {
+
+    private final Trie[] children = new Trie[26];
+    private static final Trie root = new Trie();
+    private boolean isEndOfWord;
+
+    public Trie() {
+        Arrays.fill(children, null);
+        this.isEndOfWord = false;
+    }
+
+    public void insert(String word){
+        Trie next = root;
+        for (char ch: word.toCharArray()){
+            int index = ch - 'a';
+            if (next.children[index] == null){
+                next.children[index] = new Trie();
+            }
+            next = next.children[index];
+        }
+        next.isEndOfWord = true;
+    }
+
+    public boolean search(String word){
+        Trie next = root;
+        for (char ch: word.toCharArray()) {
+            int index = ch - 'a';
+            if (next.children[index] == null){
+                return false;
+            }
+            next = next.children[index];
+        }
+        return next != null && next.isEndOfWord;
+    }
+
+    public boolean startWith(String prefix){
+        if (prefix == null) {
+            return false;
+        }
+        for (char ch: prefix.toCharArray()){
+            int index = ch - 'a';
+            if (root.children[index] == null){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Trie[] getChildren() {
+        return this.children;
+    }
+
+    public boolean isEndOfWord(){
+        return this.isEndOfWord;
+    }
+}
