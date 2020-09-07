94bf99c2a7d423f39806d89f384826061d8c1064
kaidzen <karpov.srg#gmail.com>
2020-09-07 23:26:31 +0200

    Add two strings, and do not use BigInt or embedded library for converting String to Integer. Used StringBuilder

---
+++ src/main/java/org/kaidzen/study/leetcode/AddStrings.java
@@ -0,0 +1,36 @@
+package org.kaidzen.study.leetcode;
+
+public class AddStrings {
+
+    public static void main(String[] args) {
+        AddStrings addStrings = new AddStrings();
+        String result = addStrings.addStrings("123", "45");
+        System.out.println(result);
+    }
+
+    public String addStrings(String num1, String num2) {
+        int first = num1.length()-1;
+        int second = num2.length()-1;
+        StringBuilder container = new StringBuilder();
+        int carry = 0;
+        while (first >= 0 || second >=0) {
+            int sum = 0;
+
+            if (first >= 0){
+                sum += num1.charAt(first) - '0';
+            }
+            if (second >= 0){
+                sum += num2.charAt(second) - '0';
+            }
+            container.append(sum % 10);
+            carry = sum / 10;
+
+            first--;
+            second--;
+        }
+        if (carry != 0){
+            container.append(carry);
+        }
+        return container.reverse().toString();
+    }
+}
