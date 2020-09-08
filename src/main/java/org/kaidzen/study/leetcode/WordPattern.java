d90949b27b77ee36473643379d9cae7654fe3f2f
kaidzen <karpov.srg#gmail.com>
2020-09-08 22:10:49 +0200

    Find if words represented by pattern. Use one map for mapping

---
+++ src/main/java/org/kaidzen/study/leetcode/WordPattern.java
@@ -0,0 +1,37 @@
+package org.kaidzen.study.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
+
+    public static void main(String[] args) {
+        WordPattern checkPattern = new WordPattern();
+//        Input: pattern = "abba", str = "dog cat cat dog"
+//        Output: true
+        System.out.println(checkPattern.wordPattern("abba", "dog cat cat dog"));
+
+//        Input:pattern = "abba", str = "dog cat cat fish"
+//        Output: false
+        System.out.println(checkPattern.wordPattern("abba", "dog cat cat fish"));
+    }
+
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (pattern.length() != words.length){
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+        for(int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+            if (!map.containsKey(c) && !map.containsValue(word)) {
+                map.put(c, word);
+            }
+            if (!word.equals(map.get(c))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
