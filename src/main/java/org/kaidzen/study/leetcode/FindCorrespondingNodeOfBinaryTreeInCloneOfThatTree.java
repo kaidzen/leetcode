d1984ffb03ced97fe839599b497065e9ac52548f
kaidzen <karpov.srg#gmail.com>
2021-01-03 11:27:30 +0100

    Traversing binary tree to find origin in cloned. As we looking for similar node, not value, traversion can be any kind.

---
+++ src/main/java/org/kaidzen/study/leetcode/FindCorrespondingNodeOfBinaryTreeInCloneOfThatTree.java
@@ -0,0 +1,31 @@
+package org.kaidzen.study.leetcode;
+
+import org.kaidzen.study.leetcode.util.TreeNode;
+
+public class FindCorrespondingNodeOfBinaryTreeInCloneOfThatTree {
+
+    private TreeNode target;
+    private TreeNode result;
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        this.target = target;
+        inOrder(original, cloned);
+        return this.result;
+    }
+
+    private void inOrder(TreeNode origin, TreeNode clone){
+        if (origin != null){
+            //Traversal in order -> (Left, Root, Right)
+            inOrder(origin.left, clone.left);
+            if (origin == target){
+                this.result = clone;
+            }
+            //Or
+            inOrder(origin.right, clone.right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
